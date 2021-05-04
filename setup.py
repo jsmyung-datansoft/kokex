30dbca6047a2bdfd2e9546d1640dbe6581104b7c
jsmyung-datansoft <jsmyung@datansoft.com>
2021-05-04 09:53:21 +0900

    fix #1 - pip install 시 import 오류 수정

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="kokex",
-    version="0.0.1",
+    version="0.0.2",
     author="jsmyung",
     author_email="jsmyung@datansoft.com",
     description="A Korean Keywords Extractor with Syntactic Analysis",
@@ -13,10 +13,14 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/jsmyung-datansoft/kokex",
     packages=setuptools.find_packages(),
+    python_requires='>=3.7',
+    install_requires=[
+        'konlpy>=0.5.2',
+        'networkx>=2.5.1',
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
 )
